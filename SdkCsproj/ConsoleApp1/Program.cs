dc8e3b5ca326a5757e5e219c9609f8e0556261dc
Eberhard Beilharz <eb1@sil.org>
2019-02-20 17:16:10 +0100

    Add new sdk-style csproj format projects

---
+++ SdkCsproj/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection;
+using TestGitVersionCore;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var assembly = Assembly.GetExecutingAssembly();
+			var assemblyName = assembly.GetName().Name;
+			var gitVersionInformationType = assembly.GetType("GitVersionInformation");
+			var versionField = gitVersionInformationType.GetField("InformationalVersion");
+			Console.WriteLine(versionField.GetValue(null));
+			Console.WriteLine(Class1.GetVersion());
+		}
+	}
+}
---
+++ SdkCsproj/TestGitVersionCore/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+
+namespace TestGitVersionCore
+{
+	public class Class1
+	{
+		public static string GetVersion()
+		{
+			var assembly = Assembly.GetExecutingAssembly();
+			var assemblyName = assembly.GetName().Name;
+			var gitVersionInformationType = assembly.GetType("GitVersionInformation");
+			var versionField = gitVersionInformationType.GetField("FullSemVer");
+			return (string)versionField.GetValue(null);
+		}
+	}
+}
